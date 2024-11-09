93dd3c3183735bf02fde0801ed6f2db00b02e7b8
Diyanadura Menula Sasen De Silva <rusirunavanjaya2004@gmail.com>
2024-11-09 16:20:58 +0530

    Example.java
    Java Method's Code

---
+++ Example.java
@@ -0,0 +1,25 @@
+//Example.java
+class Example{
+  public static void main(String args[]){
+    printA();
+    printB();
+    printC();
+    printD();
+    printE();
+  }
+  public static void printA(){
+    System.out.println("A");
+  }
+  public static void printB(){
+    System.out.println("B");
+  }
+  public static void printC(){
+    System.out.println("C");
+  }
+  public static void printD(){
+    System.out.println("D");
+  }
+  public static void printE(){
+    System.out.println("E");
+  }
+}
